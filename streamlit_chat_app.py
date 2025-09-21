92dd8ee3033fb11dc19c683c8cd3dd1c1c616848
wijonur25 <56059022+wijonur25@users.noreply.github.com>
2025-09-21 22:33:38 +0700

    Update streamlit_chat_app.py

--- streamlit_chat_app.py
+++ streamlit_chat_app.py
@@ -14,7 +14,7 @@ st.caption("A simple and friendly chat using Google's Gemini Flash model")
 if ("genai_client" not in st.session_state) or (getattr(st.session_state, "_last_key", None) != google_api_key):
     try:
         # If the conditions are met, create a new client.
-        st.session_state.genai_client = genai.Client(api_key=AIzaSyCiq9-aqBCaLkK5bwj6YZtQqHxhPCGz1Bc)
+        st.session_state.genai_client = genai.Client(api_key="AIzaSyCiq9-aqBCaLkK5bwj6YZtQqHxhPCGz1Bc")
         # Store the new key in session state to compare against later.
         st.session_state._last_key = google_api_key
         # Since the key changed, we must clear the old chat and message history.
